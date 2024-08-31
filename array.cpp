82470548e6749968eecff6c0e32248d91d84ee17
Shristi Dutta <shristidutta2k20@gmail.com>
2024-08-31 15:20:26 +0530

    add two numbers

---
+++ array.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int arr[10];
+    for(int i =0;i<=9;i=i+1){
+    cin >> arr[i];
+}
+
+for(int i =0;i<=9;i=i+1){
+cout << arr[i] <<" ";
+}
+return 0;
+}
+
