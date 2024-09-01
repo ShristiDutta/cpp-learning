bce139fd53322036689074b37f94aa934fc34aaa
Shristi Dutta <shristidutta2k20@gmail.com>
2024-09-01 18:56:31 +0530

    function

---
+++ functions.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+//void-> which doesnot returns anything
+void printname(){
+    cout << "hey shristi!";
+}
+    
+    int main()
+{
+    printname();
+    return 0;
+}
