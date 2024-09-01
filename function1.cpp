8db4cd87d9611d56167a2c6af42ba977b4bf9af2
Shristi Dutta <shristidutta2k20@gmail.com>
2024-09-01 18:58:18 +0530

    function

---
+++ function1.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+    void printname(string name){
+        cout << "hey" << name << endl;
+    }
+    int main()
+    {
+        string name;
+        cin >> name;
+        printname(name);
+
+        string name2;
+        cin >> name;
+        printname(name);
+
+        string name3;
+        cin >> name;
+        printname(name);
+        return 0;
+    }
