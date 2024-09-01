e6846adae8d10fd37b9ed9dadfceeaa6a5c428ec
Shristi Dutta <shristidutta2k20@gmail.com>
2024-09-01 19:03:34 +0530

    adultornot

---
+++ print_adult_or_not.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int age;
+    cin >> age;
+    if(age >=18){
+        cout << "you are an adult!";
+    }
+    else if(age<10){
+        cout << "you are not an adult!";
+    }
+    return 0;
+}
