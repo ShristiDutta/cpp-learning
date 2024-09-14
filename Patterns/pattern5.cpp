1158af33ed8ac8739114150694521ddf7837cd6b
Shristi Dutta <shristidutta2k20@gmail.com>
2024-09-15 01:15:05 +0530

    pattern

---
+++ Patterns/pattern5.cpp
@@ -0,0 +1,29 @@
+/*
+
+1
+12
+123
+1234
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int i = 1;
+    while (i <= n)
+    {
+        int j = 1;
+        while (j <= i)
+        {
+            cout << j;
+            j = j + 1;
+        }
+        cout << endl;
+        i = i + 1;
+    }
+    return 0;
+}
