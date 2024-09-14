8f4e60e0391431b08a8476f1929147fc7c3c4d15
Shristi Dutta <shristidutta2k20@gmail.com>
2024-09-14 12:33:45 +0530

    sum

---
+++ Array/sum_0f_even_numbers.cpp
@@ -0,0 +1,16 @@
+//find sum of all even numbers
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int i =2;
+    int sum=0;
+    while(i<=n) {
+        sum=sum+i;
+        i=i+2;
+    }
+    cout << sum << endl;
+    return 0;
+}
