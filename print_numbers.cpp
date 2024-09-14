ef1a6aabd07e1c94174632a5a06bc268a3ca7e0e
Shristi Dutta <shristidutta2k20@gmail.com>
2024-09-14 12:04:29 +0530

    printnumbers

---
+++ print_numbers.cpp
@@ -0,0 +1,14 @@
+//print numbers 1 to N
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>> n;
+    int i=1;
+    while(i<=n) {
+        cout<<i<<" ";
+        i=i+1;
+
+    }
+}
